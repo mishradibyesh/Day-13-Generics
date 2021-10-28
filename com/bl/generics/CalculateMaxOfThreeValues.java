1ea682ce2f690c1a65cd8715ce77edf739631168
mishradibyesh <dibyeshmishra207@gmail.com>
2021-10-28 13:03:28 +0530

    ADDED:code to find max among three integers

--- com/bl/generics/CalculateMaxOfThreeValues.java
+++ com/bl/generics/CalculateMaxOfThreeValues.java
@@ -2,8 +2,23 @@ package com.bl.generics;
 
 public class CalculateMaxOfThreeValues {
 
-	public static void main(String[] args) {
-		System.out.println("program to find maximum number among three numbers");
+	public static void main(String[] args) {// Three Integer objects initialization
+		Integer firstNumber = 54 ;
+		Integer secondNumber = 34 ;
+		Integer thirdNumber = 21 ;
+
+		Integer max = firstNumber ;
+
+		// using compareTo method
+		if(secondNumber.compareTo(max) > 0) {
+			max = secondNumber;
+		}
+		if(thirdNumber.compareTo(max) > 0) {
+			max = thirdNumber ;
+		}
+
+		// printing the maximum among three integer objects
+		System.out.println("Maximum number is "+max);
 	}
 
 }

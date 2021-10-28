b2b719973e235867b85cc54d275ce7f2c25a8a49
mishradibyesh <dibyeshmishra207@gmail.com>
2021-10-28 13:00:30 +0530

    first commit

---
+++ com/bl/generics/CalculateMaxOfThreeValues.java
@@ -0,0 +1,9 @@
+package com.bl.generics;
+
+public class CalculateMaxOfThreeValues {
+
+	public static void main(String[] args) {
+		System.out.println("program to find maximum number among three numbers");
+	}
+
+}

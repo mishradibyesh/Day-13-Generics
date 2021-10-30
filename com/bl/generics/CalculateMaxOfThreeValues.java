c6eafbcae0d03bc47e87bc5233e7c879938a8a8d
mishradibyesh <dibyeshmishra207@gmail.com>
2021-10-30 06:22:52 +0530

    ADDED : code to print the maximum value

--- com/bl/generics/CalculateMaxOfThreeValues.java
+++ com/bl/generics/CalculateMaxOfThreeValues.java
@@ -13,19 +13,21 @@ public class CalculateMaxOfThreeValues  {
 		findMax(strArr);
 	}
 
-
+	//method to find max value
 	public static <E extends Comparable<E>> void findMax(E[] arr ) {
 
 		E maximum = arr[0];
 		for(int i = 1; i < arr.length ; i++ ) {
 			if(maximum.compareTo(arr[i]) < 0) {
-				maximum = arr[i];
+				maximum = arr[i];            
 			}
 		}
 		printMax(maximum);
 	}
+
+	//print method to print the obtained max value
 	public static <E> void printMax(E max) {
-		System.out.println("Amongst the three the maximum is " + max);		
+		System.out.println("Among the three the maximum is " + max);		
 	}
 
 }
